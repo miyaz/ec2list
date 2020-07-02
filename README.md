20e4dfb362428eca33ee2a5fb96b41e6bb36519a
miyaz <miyazato@sencorp.co.jp>
2020-07-02 15:03:12 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ec2list
+
+ec2インスタンス情報をJSON形式で表示するツール
+
+## 使い方
+
+```
+./ec2list -p {profile} -r {region}
+```
+
+* profileを指定しない場合は defaultプロファイルを使用します
+
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+var (
+	profile string
+	region  string
+)
+
+func init() {
+	flag.StringVar(&profile, "p", "default", "aws shared credential profile name")
+	flag.StringVar(&region, "r", "ap-northeast-1", "aws region")
+	flag.Parse()
+}
+
+func main() {
+	sess := session.Must(session.NewSession())
+	cred := credentials.NewSharedCredentials("", profile)
+	svc := ec2.New(sess, &aws.Config{Credentials: cred,
+		Region: aws.String(region)})
+	res, err := svc.DescribeInstances(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, r := range res.Reservations {
+		for _, i := range r.Instances {
+			var instanceName string
+			for _, t := range i.Tags {
+				if *t.Key == "Name" {
+					instanceName = *t.Value
+				}
+			}
+			fmt.Printf("%s,%s,%s,%s,%s\n",
+				*i.InstanceId,
+				instanceName,
+				*i.InstanceType,
+				*i.Placement.AvailabilityZone,
+				*i.State.Name,
+			)
+		}
+	}
+}
